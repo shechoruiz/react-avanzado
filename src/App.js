b133d42ab48ed44021a7f30efb869af4534297d3
Sergio Ruiz <shecho27@gmail.com>
2020-09-28 02:03:58 -0500

    1ra parte de creación de interfaz con styled-components

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import {GlobalStyle} from './GlobalStyles'
+import {ListOfCategories} from './components/ListOfCategories'
+
+export const App = () => (
+  <>
+    <GlobalStyle />
+    <ListOfCategories />
+  </>
+)
