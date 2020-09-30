1199c3c8d0846e4d3bedf045164ee72ce86b374b
Sergio Ruiz <shecho27@gmail.com>
2020-09-29 23:06:45 -0500

    Avance en Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { Category } from "../Category";
 
 import {List, Item} from './styles'
 
-import {categories} from '../../../api/db.json'
-
 export const ListOfCategories = () => {
-  return (
-    <List>
+  const [categories, setCategories] = useState([]);
+  const [showFixed, setShowFixed] = useState(false);
+
+  useEffect(function() {
+    window.fetch("https://petgram-server-sergio.vercel.app/categories")
+      .then(res => res.json())
+      .then (response => {
+        setCategories(response)
+      })
+  }, [])
+
+  useEffect(function() {
+    const onScroll = e => {
+      const newShowFixed = window.scrollY > 200
+      showFixed !== newShowFixed && setShowFixed(newShowFixed);
+    }
+
+    document.addEventListener('scroll', onScroll)
+    return () => document.removeEventListener('scroll', onScroll)
+  }, [showFixed])
+
+  const renderList = (fixed) => (
+    <List className={fixed ? 'fixed' : ''}>
       {
         categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
       }
     </List>
   )
+
+  return (
+    <>
+      {renderList()}
+      {showFixed && renderList(true)}
+    </>
+  );
 }
