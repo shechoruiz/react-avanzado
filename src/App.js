5af4ee01c1f60c1ecfb744b37abd5acc76f3f0e1
Sergio Ruiz <shecho27@gmail.com>
2020-09-29 21:57:36 -0500

    Finalización de la creación de interfaz con styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react'
 
-import {GlobalStyle} from './GlobalStyles'
+import {GlobalStyle} from './styles/GlobalStyles'
 import {ListOfCategories} from './components/ListOfCategories'
+import { ListOfPhotoCards } from "./components/ListOfPhotoCards";
+import { Logo } from "./components/Logo";
 
 export const App = () => (
   <>
     <GlobalStyle />
+    <Logo />
     <ListOfCategories />
+    <ListOfPhotoCards />
   </>
 )
