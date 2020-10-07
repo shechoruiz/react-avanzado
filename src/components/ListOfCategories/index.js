7904b895caf342faa807e7c530373ed63e90e624
Sergio Ruiz <sergio.ruiz@edemco.co>
2020-10-07 14:21:53 -0500

    Avance del módulo Hooks

--- src/components/ListOfCategories/index.js
+++ src/components/ListOfCategories/index.js
@@ -1,37 +1,58 @@
-import React, {useState, useEffect} from 'react'
+import React, { useState, useEffect } from "react";
 import { Category } from "../Category";
 
-import {List, Item} from './styles'
+import { List, Item } from "./styles";
+
+function useCategoriesData() {
+  const [categories, setCategories] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(function () {
+    setLoading(true);
+    window
+      .fetch("https://petgram-server-sergio.vercel.app/categories")
+      .then((res) => res.json())
+      .then((response) => {
+        setCategories(response);
+        setLoading(false);
+      });
+  }, []);
+
+  return { categories, loading };
+}
 
 export const ListOfCategories = () => {
-  const [categories, setCategories] = useState([]);
   const [showFixed, setShowFixed] = useState(false);
+  const { categories, loading } = useCategoriesData();
 
-  useEffect(function() {
-    window.fetch("https://petgram-server-sergio.vercel.app/categories")
-      .then(res => res.json())
-      .then (response => {
-        setCategories(response)
-      })
-  }, [])
+  useEffect(
+    function () {
+      const onScroll = (e) => {
+        const newShowFixed = window.scrollY > 200;
+        showFixed !== newShowFixed && setShowFixed(newShowFixed);
+      };
 
-  useEffect(function() {
-    const onScroll = e => {
-      const newShowFixed = window.scrollY > 200
-      showFixed !== newShowFixed && setShowFixed(newShowFixed);
-    }
-
-    document.addEventListener('scroll', onScroll)
-    return () => document.removeEventListener('scroll', onScroll)
-  }, [showFixed])
+      document.addEventListener("scroll", onScroll);
+      return () => document.removeEventListener("scroll", onScroll);
+    },
+    [showFixed]
+  );
 
   const renderList = (fixed) => (
-    <List className={fixed ? 'fixed' : ''}>
-      {
-        categories.map((category) => <Item key={category.id}><Category {...category} /></Item>)
-      }
+    <List fixed={fixed}>
+      {loading ? (
+        <Item key="loading">
+          <Category />
+        </Item>
+      ) : (
+        categories.map((category) => (
+          <Item key={category.id}>
+            <Category {...category} />
+          </Item>
+        ))
+      )}
     </List>
-  )
+  );
 
   return (
     <>
@@ -39,4 +60,4 @@ export const ListOfCategories = () => {
       {showFixed && renderList(true)}
     </>
   );
-}
+};
--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,20 +1,43 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from "react";
 
-import {ImgWrapper, Img, Button} from './styles'
-const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-import {MdFavoriteBorder} from 'react-icons/md'
+import { ImgWrapper, Img, Button, Article } from "./styles";
+const DEFAULT_IMAGE =
+  "https://images.unsplash.com/photo-1518791841217-8f162f1e1131?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60";
+import { MdFavoriteBorder } from "react-icons/md";
 
-export const PhotoCard = ({id, likes = 0, src = DEFAULT_IMAGE}) => {
+export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
+  const element = useRef(null);
+  const [show, setShow] = useState(false);
+
+  useEffect(
+    function () {
+      const observer = new window.IntersectionObserver(function (entries) {
+        const { isIntersecting } = entries[0];
+        if (isIntersecting) {
+          console.log("si");
+          setShow(true);
+          observer.disconnect();
+        }
+      });
+      observer.observe(element.current);
+    },
+    [element]
+  );
+
   return (
-    <article>
-      <a href={`/detail/${id}`}>
-        <ImgWrapper>
-          <Img src={src} alt=""/>
-        </ImgWrapper>
-      </a>
-      <Button>
-        <MdFavoriteBorder size='32px' /> {likes} likes!
-      </Button>
-    </article>
-  )
-}
+    <Article ref={element}>
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt="" />
+            </ImgWrapper>
+          </a>
+          <Button>
+            <MdFavoriteBorder size="32px" /> {likes} likes!
+          </Button>
+        </>
+      )}
+    </Article>
+  );
+};
